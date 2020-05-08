cce190e10543bfcb93e9e8288865d630f6d84641
Manu Farcis <emmanuel@farcis.fr>
2020-05-08 09:38:22 +0200

    modify my dwmblocks

--- config.h
+++ config.h
@@ -2,18 +2,20 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	/* {"",	"crypto",	0,	13}, */
-	{"",	"torrent",	20,	7},
-	{"",	"news",		0,	6},
-	/* {"",	"moonphase",	18000,	5}, */
-	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	0},
-	{"",	"clock",	60,	0},
-	{"",	"internet",	5,	4},
+	{"",	"spotifyblock",		0,	12},
+	{"",	"layout",		0,	11},
+	{"",	"pacpackages",		0,	8},
+	/*{"",	"crypto",		0,	13}, */
+/*	{"",	"torrent",		20,	7},*/
+	{"",	"news",			0,	6},
+	/* {"",	"moonphase",		18000,	5}, */
+	{"",	"weather",		18000,	5},
+	/*{"",	"mailbox",		180,	12},*/
+	{"",	"volume",		0,	10},
+	/*{"",	"battery",		5,	0},*/
+	{"",	"clock",		60,	0},
+	{"",	"internet",		5,	4},
+	/*{"",	"shutdown_menu",	0,	4},*/
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
